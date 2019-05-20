7d73ce64c11069c5eca55c0ec18a6537352c2b80
zizwang <40439322+zizwang@users.noreply.github.com>
2019-05-20 20:55:18 +0800

    Add files via upload

--- menu-screen.js
+++ menu-screen.js
@@ -5,36 +5,33 @@
 var s = document.getElementById('song-selector');
 var t = document.getElementById('query-input');
 var e = document.getElementById('error');
-
+var theme = ['candy', 'charlie brown', 'computers', 'dance', 'donuts', 'hello kitty', 'flowers', 'nature', 'turtles', 'space'];
 class MenuScreen {
-    constructor(containerElement) {
-        // TODO(you): Implement the constructor and add fields as necessary.
-        this.containerElement = containerElement;
+  constructor(containerElement) {
+    this.containerElement = containerElement;
 
-        fetch('https://fullstackccu.github.io/homeworks/hw4/songs.json')
-            .then(onResponse)
-            .then(onDataReady);
+    // song
+    fetch('https://fullstackccu.github.io/homeworks/hw4/songs.json')
+      .then(onResponse)
+      .then(onDataReady);
 
-        let theme = ['candy', 'charlie brown', 'computers', 'dance', 'donuts', 
-                     'hello kitty', 'flowers', 'nature', 'turtles', 'space'];
-        
-        t.value = theme[Math.floor(Math.random() * 10)];
+    //theme
+    
+    t.value = theme[Math.floor(Math.random()*10)];
 
-        this.goBtn = document.getElementById("go");
-        this.goBtn = addEventListener('submit',this.onClick);
-    }
-    // TODO(you): Add methods as necessary.
-    onClick(event) {
-        event.preventDefault();
-        url = "https://api.giphy.com/v1/gifs/search?q="+ t.value +"&api_key=VZ7FFJUrfrJrkWSEGvKsVtBp6wNRiyxZ";
-        app.music.gif.fetch_gif(url);
-    }
-
-    show() {
-        this.containerElement.classList.remove('inactive');
-    }
-
-    hide() {
-        this.containerElement.classList.add('inactive');
-    }
+    //submit
+    this.goBtn = document.getElementById("go");
+    this.goBtn = addEventListener('submit',this.onClick);
+  }
+  onClick(event){
+    event.preventDefault();
+    url = "https://api.giphy.com/v1/gifs/search?q="+ t.value +"&api_key=VZ7FFJUrfrJrkWSEGvKsVtBp6wNRiyxZ";
+    app.music.gif.fetch_gif(url);
+  }
+  show(){
+    this.containerElement.classList.remove('inactive');
+  }
+  hide() {
+    this.containerElement.classList.add('inactive');
+  }
 }
